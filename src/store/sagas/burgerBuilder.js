76dcb872843945e4e477d449e4f5520a2d6a2576
Reaver <reaver@Slava-Mac.local>
2019-08-20 22:37:19 +0600

    Completed

---
+++ src/store/sagas/burgerBuilder.js
@@ -0,0 +1,13 @@
+import axios from '../../axios-orders';
+import { put } from 'redux-saga/effects';
+import * as actions from '../actions';
+export function* initIngredientsSaga() {
+  try {
+    const response = yield axios.get(
+      'https://react-burgerbuilder1.firebaseio.com/ingredients.json'
+    );
+    yield put(actions.setIngredients(response.data));
+  } catch (error) {
+    yield put(actions.fetchIngredientsFailed());
+  }
+}
